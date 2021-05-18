42f4938cd7a7d0571fd3f82189237581f4a2a6b6
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-17 22:55:16 -0400

    Add files via upload

---
+++ LastRoundCycleLoader.java
@@ -0,0 +1,32 @@
+//What is claimed:for stack cycling
+var 1.0="lastLinkedAssembly";
+const 1.1= "clearSpacerCartridgeCombination";
+public class FireStack extends Object{
+new FireStack = myObj fireStack;
+public static ReloadStack(protected int DummyStackViewA=0;
+protected int DummyStackViewB=DummyStackViewA=+1;
+protected void int looselink(""DummyCartrigeA""=+""DummyCartrigeB"");
+public void int spreaderCartrige=protected void int looselink()++=protected void int doubleStack(){
+protected static void class magWell{
+Reload.magWell(void Cycle)=Firearm()+++A));
+@overrides FireStack
+public static void  class  multi_Cartrige extends doubleStack implements looselink{
+new multi_Cartrige = myObj Multi_Cartrige;
+myObj.FireStack.multi_Cartrige(void looselink){this.Multi_Cartrige(){looselink==""DummyCartrigeA""
++++""DummyCartrigeB""};
+public boolean withOutletY=true;
+public boolean withOutletN=false;
+public static void class Reload{
+new Reload =myObj reload;
+myObj.FireStack.reload(int StartReload --=1){int StartReload<=FireStack--=DummyStackB>=0);
+@Overrides FireStack
+protected static void class feedClipAssembly{
+new feedClipAssembly = myObj FeedClipAssembly;
+myObj.FeedClipAssembly(int FeedClip){this.FeedClipAssembly(){FeedClip=<
+(""DummyStackA""+++""DummyStackB"")
+-=""1""<=
+(""DummyStackA""+++""DummyStackB"")+=""1""};
+protected static int taper=""DummyStackA""=+1/""DummyStackB""=+2
+protected static void singleStack=""FeedClipAssembly""-=2/""FeedClipAssembly""-=1--=1;
+public static const exitStack=""singleStack""\=0;
+
