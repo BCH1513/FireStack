2ac96275de6661476c66691f41b4cfb678185c22
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-05-17 23:02:31 -0400

    Update LastRoundCycleLoader.java

--- LastRoundCycleLoader.java
+++ LastRoundCycleLoader.java
@@ -1,24 +1,38 @@
 //What is claimed:for stack cycling
-var 1.0="lastLinkedAssembly";
-const 1.1= "clearSpacerCartridgeCombination";
+import com.java.base.*;
+import com.java.base.Object;
+
+Public static void main(String[],args){
 public class FireStack extends Object{
 new FireStack = myObj fireStack;
+
 public static ReloadStack(protected int DummyStackViewA=0;
+
 protected int DummyStackViewB=DummyStackViewA=+1;
-protected void int looselink(""DummyCartrigeA""=+""DummyCartrigeB"");
+
+protected int looselink(""DummyCartrigeA""=+""DummyCartrigeB"");
+
 public void int spreaderCartrige=protected void int looselink()++=protected void int doubleStack(){
+@override FireStack
+
 protected static void class magWell{
 Reload.magWell(void Cycle)=Firearm()+++A));
+
 @overrides FireStack
 public static void  class  multi_Cartrige extends doubleStack implements looselink{
 new multi_Cartrige = myObj Multi_Cartrige;
 myObj.FireStack.multi_Cartrige(void looselink){this.Multi_Cartrige(){looselink==""DummyCartrigeA""
 +++""DummyCartrigeB""};
+
 public boolean withOutletY=true;
+
 public boolean withOutletN=false;
+
+@ovveride FireStack
 public static void class Reload{
 new Reload =myObj reload;
 myObj.FireStack.reload(int StartReload --=1){int StartReload<=FireStack--=DummyStackB>=0);
+
 @Overrides FireStack
 protected static void class feedClipAssembly{
 new feedClipAssembly = myObj FeedClipAssembly;
@@ -26,7 +40,12 @@ myObj.FeedClipAssembly(int FeedClip){this.FeedClipAssembly(){FeedClip=<
 (""DummyStackA""+++""DummyStackB"")
 -=""1""<=
 (""DummyStackA""+++""DummyStackB"")+=""1""};
+
 protected static int taper=""DummyStackA""=+1/""DummyStackB""=+2
+
 protected static void singleStack=""FeedClipAssembly""-=2/""FeedClipAssembly""-=1--=1;
-public static const exitStack=""singleStack""\=0;
+
+public static const exitStack=""singleStack""\=0;}
+return FireStack();
+};
 
