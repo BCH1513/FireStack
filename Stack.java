c1850916684dfcf24ec382093d5b031891a6d83f
BCH1513 <40265579+BCH1513@users.noreply.github.com>
2021-06-20 14:10:38 -0400

    Add files via upload

---
+++ Stack.java
@@ -0,0 +1,12 @@
+public static ALPHABET[]
+{"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"}==
+public static []ALPHABET[]
+={"Z","Y","X","W","V","U","T","S","R","Q","P","O","L","N","M","L","K","J","I","H","G","F","E","D","C","B","A"}==
+public static []alphabet[]=
+{"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"}==
+public static []alphabet[]
+={"z","y","x","w","v","u","t","s","r","q","p","o","n","m","l","k","j","i","h","g","f","e","d","c","b","a"};
+public static decimal[]
+{'1','2','3','4','5','6','7','8'\, '9','0'}=
+public static []decimal[]
+=={'1','2','3','4','5','6','7','8','9','0'};
